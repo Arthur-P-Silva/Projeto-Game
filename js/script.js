ebf54f09ebf494e453f22fc8453a2b61df5fe031
Arthur Pereira da Silva <arthurpsilva1@gmail.com>
2025-07-24 23:52:37 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const nuvem = document.querySelector('.nuvem');
+
+const game = document.querySelector('.game-board');
+
+const jump = () =>{
+    mario.classList.add('jump');
+
+    setTimeout(() =>{ 
+
+        mario.classList.remove('jump');
+
+    }, 500);
+}
+
+const loop = setInterval(() =>{
+
+    const pipePosition = pipe.offsetLeft; //Deslocamento esquerdo
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); //Pega o estilo computado na imagem do mario (no caso pega o bottom), replace serve para apagar o px, + serve para converter parar Number
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+
+        let som = new Audio('./img/sommorrendo.mp3');
+        som.play();
+
+        pipe.style.animation = 'none'; //Desliga animação
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none'; //Desliga animação
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './img/mariogameover.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        nuvem.style.animationPlayState = 'paused'; //Para a animação
+
+
+        clearInterval(loop); //loop para de rodar
+    }
+
+}, 10);
+
+document.addEventListener('keydown', jump);
+document.addEventListener('touchstart', jump);
+
+const bntReiniciar = document.getElementById('reset');
+
+bntReiniciar.addEventListener('click', () =>{
+    location.reload(); //Recarrega a página inteira
+});
